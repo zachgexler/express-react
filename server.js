29b32326063f173a8c343cf360973bc0331e3d45
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-09-26 14:40:32 -0400

    fixing JSON

--- server.js
+++ server.js
@@ -22,22 +22,22 @@ const morgan = require("morgan");
 ////////////////////////////////
 // Establish Connection
 mongoose.connect(MONGODB_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 // Connection Events
 mongoose.connection
-    .on("open", () => console.log("You are connected to mongoose"))
-    .on("close", () => console.log("You are disconnected from mongoose"))
-    .on("error", (error) => console.log(error));
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
 
 ///////////////////////////////
 // MODELS
 ////////////////////////////////
 const PeopleSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    title: String,
+  name: String,
+  image: String,
+  title: String,
 });
 
 const People = mongoose.model("People", PeopleSchema);
@@ -54,29 +54,53 @@ app.use(express.json()); // parse json bodies
 ////////////////////////////////
 // create a test route
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
 });
 
 // PEOPLE INDEX ROUTE
 app.get("/people", async (req, res) => {
-    try {
-        // send all people
-        res.json(await People.find({}));
-    } catch (error) {
-        //send error
-        res.status(400).json(error);
-    }
+  try {
+    // send all people
+    res.json(await People.find({}));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
 });
 
 // PEOPLE CREATE ROUTE
 app.post("/people", async (req, res) => {
-    try {
-        // send all people
-        res.json(await People.create(req.body));
-    } catch (error) {
-        //send error
-        res.status(400).json(error);
-    }
+  try {
+    // send all people
+    res.json(await People.create(req.body));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+// PEOPLE DELETE ROUTE
+app.delete("/people/:id", async (req, res) => {
+  try {
+    // send all people
+    res.json(await People.findByIdAndRemove(req.params.id));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+// PEOPLE UPDATE ROUTE
+app.put("/people/:id", async (req, res) => {
+  try {
+    // send all people
+    res.json(
+      await People.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
 });
 
 ///////////////////////////////
